d7fbae964847e2179280a7cf35b3ebbd200ad249
魏凯凯 <137067401@qq.com>
2014-10-17 09:28:08 +0800

    sadf

---
+++ KarBao/KarBao/controllers/cards/CardCell.h
@@ -0,0 +1,18 @@
+//
+//  CardCell.h
+//  KarBao
+//
+//  Created by Caland on 14-10-16.
+//  Copyright (c) 2014年 Caland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardCell : UITableViewCell
+
+@property (nonatomic, retain) IBOutlet UILabel *nameLabel;
+
+@property (nonatomic, retain) IBOutlet UILabel *cardLabel;
+
+
+@end
---
+++ KarBao/KarBao/controllers/cards/CardCell.m
@@ -0,0 +1,23 @@
+//
+//  CardCell.m
+//  KarBao
+//
+//  Created by Caland on 14-10-16.
+//  Copyright (c) 2014年 Caland. All rights reserved.
+//
+
+#import "CardCell.h"
+
+@implementation CardCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
